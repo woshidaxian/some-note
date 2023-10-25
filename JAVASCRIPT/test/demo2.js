f15a5482a8280ea1613525698c355f9525e50615
woshidaxian <huang15057081701@163.com>
2023-10-25 18:56:12 +0800

    1

--- JAVASCRIPT/test/demo2.js
+++ JAVASCRIPT/test/demo2.js
@@ -1,24 +1,5 @@
-let a = 1
-let b = '1'
-let c = [1,2]
-let d = {
-  a: 1,
-  b: 2
-}
-let e = undefined
-let f = null
-let j = true
-let h = function () {}
-console.log(typeof a)
-console.log(typeof b)
-console.log(typeof c)
-console.log(typeof d)
-console.log(typeof e)
-console.log(typeof f)
-console.log(typeof j)
-console.log(typeof h)
-console.log('********************************')
-console.log(a instanceof Number)
-console.log(b instanceof String)
-console.log(c instanceof Array)
-console.log(d instanceof Object)
+const a = [1,2,10,6,7,3]
+a.reduce((prev, cur, index, arr)=>{
+  console.log(prev, cur, index, arr)
+  return (prev+cur).toString()
+})
