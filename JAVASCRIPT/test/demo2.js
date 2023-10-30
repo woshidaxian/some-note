4fe72262df042570691d7b955d4ba75d5dd329bd
woshidaxian <huang15057081701@163.com>
2023-10-30 14:56:34 +0800

    js

--- JAVASCRIPT/test/demo2.js
+++ JAVASCRIPT/test/demo2.js
@@ -1,5 +1,5 @@
-const a = [1,2,10,6,7,3]
-a.reduce((prev, cur, index, arr)=>{
-  console.log(prev, cur, index, arr)
-  return (prev+cur).toString()
-})
+function  a(a, b) {
+  console.log(arguments)
+}
+
+a(1, 2)
